1c92ebbbdd922d3c6488b17beca98cef16406862
minju <betaa0528@gmail.com>
2023-10-31 22:05:44 +0900

    #21 - 댓글 서비스 로직의 테스트와 골격 잡기
    댓글과 관련된 단순 crud 기능을 테스트로 표현
    강의 시간 상 기능의 일부만 테스트로 표현함
    나머지 필요 기능은 별도 작성하여 넣기로.

---
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/dto/ArticleCommentDto.java
@@ -0,0 +1,20 @@
+package com.fastcampus.projectboard.dto;
+
+import com.fastcampus.projectboard.domain.ArticleComment;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO for {@link ArticleComment}
+ */
+public record ArticleCommentDto(
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy,
+        String content
+) {
+    public static ArticleCommentDto of(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String content) {
+        return new ArticleCommentDto(createdAt, createdBy, modifiedAt, modifiedBy, content);
+    }
+}
